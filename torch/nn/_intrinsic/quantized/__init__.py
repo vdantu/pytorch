471d393533679d498733a88fa6b0369a8b5d8e62
Zafar Takhirov <cc.rafaz@zafar.cc>
2019-08-19 13:44:27 -0700

    AddReLU module
    ghstack-source-id: f64662867e26063914c145ffd2551ad67945f4ed
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/24217

--- torch/nn/_intrinsic/quantized/__init__.py
+++ torch/nn/_intrinsic/quantized/__init__.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-from .modules import LinearReLU
+
+from .modules import AddReLU
 from .modules import ConvReLU2d
+from .modules import LinearReLU
 
 __all__ = [
+    'AddReLU',
     'LinearReLU',
     'ConvReLU2d',
 ]
